b6a137d9f487391ca52494003be0274cb99bd13e
Victor Monteiro <victor.hms1206@gmail.com>
2024-08-28 11:30:03 -0300

    Primeiro commit
    Iniciando o projeto e definindo ideias/conceitos iniciais

---
+++ trabalhoAlgePorgII(cavalo).c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 4; // defini pq acho que da para usar mais pra frente(ideia minha), se não precisar colocamos como nota/apagamos
+
+    //movimentos possíveis do cavalo no xadrez, primeiro número indica o movimento vertical e o segundo o movimento horizontal
+    int movimentosCavalo[8][2] = {
+        {2,1}, {1,2}, {-1,2}, {-2, 1},
+        {-2, -1}, {-1, -2}, {-2, 1}, {2,1};
+    }
