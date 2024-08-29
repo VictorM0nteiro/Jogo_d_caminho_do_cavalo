0ab109e4efbb7a447a17e5a34598e0c97e0ae979
Matheus <matheusn06leite@gmail.com>
2024-08-29 13:35:24 -0300

    Definindo quais as novas posicoes sao possiveis para o cavalo atraves da funcao movimento_valido

--- trabalhoAlgePorgII(cavalo).c
+++ trabalhoAlgePorgII(cavalo).c
@@ -2,34 +2,76 @@
 #include <stdlib.h>
 #include <time.h>
 
-//#define N 4 // na minha máquina não está funcionando a definição global, vou ter que fazer manuelmente int N = 4 todas as vezes
+#define TAMANHO 4 // defini o tamanho do tabuleiro
 
-    //movimentos possíveis do cavalo no xadrez, primeiro número indica o movimento vertical e o segundo o movimento horizontal
-    int movimentosCavalo[8][2] = {
-        {2,1}, {1,2}, {-1,2}, {-2, 1},
-        {-2, -1}, {-1, -2}, {-2, 1}, {2,1}
+//funcao que imprime o tabuleiro
+void imprime_tabuleiro(int tabuleiro[TAMANHO][TAMANHO]){
+    for (int i = 0; i < TAMANHO; i++)
+    {
+        for (int j = 0; j < TAMANHO; j++)
+        {
+            printf("%2d ",tabuleiro[i][j]);
+        }
+        printf("\n"); 
+    }
+}
+
+//funcao para verificar se a nova posicao que o cavalo pode assumir eh valida
+int movimento_valido(int nova_linha, int nova_coluna, int tabuleiro[TAMANHO][TAMANHO]){
+    //nova_linha e nova_coluna tem que ser positivo pois se nao pode acessar uma posicao de memoria indevida
+    if(nova_linha >=0 && nova_linha < TAMANHO && nova_coluna >=0 && nova_coluna < TAMANHO && tabuleiro[nova_linha][nova_coluna] == 0) {
+        return 1; //a nova posicao eh valida
+    }
+    else return 0;
+}
+
+int main(){
+    int tabuleiro[TAMANHO][TAMANHO] = {0}; //criando tabuleiro com todas as casas com 0
+
+    // movimentos possíveis do cavalo no xadrez, primeiro número indica o movimento vertical e o segundo o movimento horizontal
+    int movimentos_Cavalo[8][2] = {
+        {2, 1}, {2, -1},
+        {-2, 1}, {-2, -1},
+        {1, 2}, {1, -2},
+        {-1, 2}, {-1, -2}
     };
 
-    void imprimirTabuleiro(int *tabuleiro, int N){
-        printf("\nTabuleiro\n");
-        for( int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                printf("%2d ", *(tabuleiro + (i * N) + j)); //%2d serve para melhorar a estética da impressão // (tabuleiro) + (i * N): Depois, a multiplicação é somada ao ponteiro    tabuleiro. Calculcando a posição de memória correspondente a linha i do tabuleiro, depois e mesmo com J para a coluna;
+    //variaveis da posicao inicial do cavalo
+    int cavalo_linha, cavalo_coluna;
+
+
+    //colocando a posicao inicial aleatoria
+    srand(time(NULL));  //gera um numero aleatorio toda vez que o tempo muda
+    cavalo_linha = rand() % TAMANHO;
+    cavalo_coluna = rand() % TAMANHO;
+    tabuleiro[cavalo_linha][cavalo_coluna] = 1; //colocando o cavalo no tabuleiro
+
+    //criando o ponteiro
+    int *P = &tabuleiro[cavalo_linha][cavalo_coluna];
+
+    //criando um contador de casas preenchidas para verificar se o usuário ganhou ou perdeu
+    int cont_casas=1;
+
+    // while (1)
+    // {
+        printf("Tabuleiro atual: \n");
+        imprime_tabuleiro(tabuleiro);
+
+        //definindo os movimentos possíveis a partir da posicao atual do cavalo
+        for (int i = 0; i < 8; i++)
+        {
+            //criando as variavies que vao receber as novas posicoes que o cavalo pode assumir
+            int nova_linha = cavalo_linha + movimentos_Cavalo[i][0];
+            int nova_coluna = cavalo_coluna + movimentos_Cavalo[i][1];
+
+            //precisamos verificar se a nova posicao eh valida (esta dentro do tabuleiro e se a posicao nao foi preenchida ainda)
+            if(movimento_valido(nova_linha, nova_coluna, tabuleiro)){
+                printf("Valido linha: %d e coluna: %d\n",nova_linha,nova_coluna);
+
             }
-            printf("\n");
+
         }
-    }
-
-    int main(){
-<<<<<<< HEAD
-        int N = 4;
-        int tabuleiro[4][4] = {0};
-        
-        tabuleiro[0][0] = 1;
-
-        imprimirTabuleiro((int*)tabuleiro, N);
-        //printf("Bom dia amiguinhos!(.)(.)(|)\n");
-=======
-        printf("Bom dia amiguinhoooooos!(.)(.)(|)\n");
->>>>>>> f86180226c826e9a3d26d55e6b8bf4242489a2b1
-    }
+    // }
+    
+    return 0;
+}
