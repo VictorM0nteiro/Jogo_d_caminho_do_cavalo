f1c0b35ec25ad2b4e49ec8ace6e8f733d59dc0f8
Matheus <matheusn06leite@gmail.com>
2024-08-29 11:44:36 -0300

    Criado o tabuleiro do jogo, e definindo a posicao do cavalo como aleatoria

--- trabalhoAlgePorgII(cavalo).c
+++ trabalhoAlgePorgII(cavalo).c
@@ -2,14 +2,51 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 4; // defini pq acho que da para usar mais pra frente(ideia minha), se não precisar colocamos como nota/apagamos
+#define TAMANHO 4 // defini o tamanho do tabuleiro
 
-    //movimentos possíveis do cavalo no xadrez, primeiro número indica o movimento vertical e o segundo o movimento horizontal
-    int movimentosCavalo[8][2] = {
+    
+    
+
+//funcao que imprime o tabuleiro
+void imprime_tabuleiro(int tabuleiro[TAMANHO][TAMANHO]){
+    for (int i = 0; i < TAMANHO; i++)
+    {
+        for (int j = 0; j < TAMANHO; j++)
+        {
+            printf("%2d ",tabuleiro[i][j]);
+        }
+        printf("\n"); 
+    }
+}
+
+
+int main(){
+    int tabuleiro[TAMANHO][TAMANHO] = {0}; //criando tabuleiro com todas as casas com 0
+
+    // movimentos possíveis do cavalo no xadrez, primeiro número indica o movimento vertical e o segundo o movimento horizontal
+    int movimentos_Cavalo[8][2] = {
         {2,1}, {1,2}, {-1,2}, {-2, 1},
         {-2, -1}, {-1, -2}, {-2, 1}, {2,1}
     };
 
-    int main(){
-        printf("Bom dia amiguinhoooooos!(.)(.)(|)\n");
+    //variaveis da posicao inicial do cavalo
+    int cavalo_x, cavalo_y;
+
+    //colocando a posicao inicial aleatoria
+    srand(time(NULL));  //gera um numero aleatorio toda vez que o tempo muda
+    cavalo_x = rand() % TAMANHO;
+    cavalo_y = rand() % TAMANHO;
+    tabuleiro[cavalo_x][cavalo_y] = 1; //colocando o cavalo no tabuleiro
+
+    while (1)
+    {
+        printf("Tabuleiro atual: \n");
+        imprime_tabuleiro(tabuleiro);
     }
+    
+    
+    
+
+
+    return 0;
+}
