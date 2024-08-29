8297a133e13c1ced4799e97bd568858a1988a189
Victor Monteiro <victor.hms1206@gmail.com>
2024-08-29 11:00:32 -0300

    Commit de desenvolvimeto
    Desenvolvendo a estrutura inicial do tabuleiro e do jogo

--- trabalhoAlgePorgII(cavalo).c
+++ trabalhoAlgePorgII(cavalo).c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define N 4; // defini pq acho que da para usar mais pra frente(ideia minha), se não precisar colocamos como nota/apagamos
+//#define N 4 // na minha máquina não está funcionando a definição global, vou ter que fazer manuelmente int N = 4 todas as vezes
 
     //movimentos possíveis do cavalo no xadrez, primeiro número indica o movimento vertical e o segundo o movimento horizontal
     int movimentosCavalo[8][2] = {
@@ -10,6 +10,22 @@
         {-2, -1}, {-1, -2}, {-2, 1}, {2,1}
     };
 
+    void imprimirTabuleiro(int *tabuleiro, int N){
+        printf("\nTabuleiro\n");
+        for( int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                printf("%2d ", *(tabuleiro + (i * N) + j)); //%2d serve para melhorar a estética da impressão // (tabuleiro) + (i * N): Depois, a multiplicação é somada ao ponteiro    tabuleiro. Calculcando a posição de memória correspondente a linha i do tabuleiro, depois e mesmo com J para a coluna;
+            }
+            printf("\n");
+        }
+    }
+
     int main(){
-        printf("Bom dia amiguinhos!(.)(.)(|)\n");
+        int N = 4;
+        int tabuleiro[4][4] = {0};
+        
+        tabuleiro[0][0] = 1;
+
+        imprimirTabuleiro((int*)tabuleiro, N);
+        //printf("Bom dia amiguinhos!(.)(.)(|)\n");
     }
